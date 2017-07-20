b15554c6803ea83d8e518008b56349c70d77f8d2
ojjsaw <ojjsaw@gmail.com>
2017-07-19 22:41:46 -0700

    multi scene enabled and racing and home scene

---
+++ MobileVRInputSDK/Assets/Samples/Scripts/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class HomeController : MonoBehaviour, IPointerDownHandler
+{
+    public enum Panels
+    {
+        PANEL1, PANEL2, PANEL3, NONE
+    }
+
+    private Panels currPanel = Panels.NONE;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        switch(currPanel)
+        {
+            case Panels.PANEL1:
+                SceneManager.LoadScene("CarRacing");
+                break;
+            case Panels.PANEL2:
+                SceneManager.LoadScene("CarRacing");
+                break;
+            case Panels.PANEL3:
+                SceneManager.LoadScene("CarRacing");
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void Panel1()
+    {
+        currPanel = Panels.PANEL1;
+    }
+
+    public void Panel2()
+    {
+        currPanel = Panels.PANEL2;
+    }
+
+    public void Panel3()
+    {
+        currPanel = Panels.PANEL3;
+    }
+
+    public void PanelExit()
+    {
+        currPanel = Panels.NONE;
+    }
+}
